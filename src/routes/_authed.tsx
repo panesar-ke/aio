170e61215751140f6df4a4c423801ab7f210b36c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-07-12 15:57:52 +0300

    style: add padding top

--- src/routes/_authed.tsx
+++ src/routes/_authed.tsx
@@ -58,7 +58,7 @@ function RouteComponent() {
             />
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="flex flex-1 flex-col gap-4 p-4">
           <Outlet />
         </div>
       </SidebarInset>
