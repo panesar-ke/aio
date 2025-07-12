28f0ac1a57dda878f9320dae913df682bda55078
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-07-12 17:16:40 +0300

    style: set max width

--- src/routes/_authed.tsx
+++ src/routes/_authed.tsx
@@ -58,7 +58,7 @@ function RouteComponent() {
             />
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">
+        <div className="flex flex-1 flex-col gap-4 max-w-6xl mx-auto w-full py-4">
           <Outlet />
         </div>
       </SidebarInset>
