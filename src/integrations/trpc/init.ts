7ab58c0a6636c15b48786f1a03828eba149d532f
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-07-12 10:47:14 +0300

    feat: add protected proc

--- src/integrations/trpc/init.ts
+++ src/integrations/trpc/init.ts
@@ -1,5 +1,6 @@
-import { initTRPC } from '@trpc/server'
+import { TRPCError, initTRPC } from '@trpc/server'
 import superjson from 'superjson'
+import { useAppSession } from '@/lib/session'
 
 const t = initTRPC.create({
   transformer: superjson,
@@ -7,3 +8,16 @@ const t = initTRPC.create({
 
 export const createTRPCRouter = t.router
 export const publicProcedure = t.procedure
+export const protectedProcedure = publicProcedure.use(async ({ ctx, next }) => {
+  const { data } = await useAppSession()
+  if (!data.id) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' })
+  }
+
+  return next({
+    ctx: {
+      ...ctx,
+      user: data,
+    },
+  })
+})
