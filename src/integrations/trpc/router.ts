10775276e28bdba2c6498763d5b12f4dc3298e42
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-07-12 16:04:45 +0300

    feat: add procurement procs

--- src/integrations/trpc/router.ts
+++ src/integrations/trpc/router.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter, protectedProcedure } from '@/integrations/trpc/init'
 import db from '@/drizzle/db'
+import { procurementProcedures } from '@/features/procurement/procedures'
 
 export const trpcRouter = createTRPCRouter({
   forms: protectedProcedure.query(async () => {
@@ -9,5 +10,6 @@ export const trpcRouter = createTRPCRouter({
       orderBy: (forms, { asc }) => [asc(forms.moduleId), asc(forms.menuOrder)],
     })
   }),
+  procurement: procurementProcedures,
 })
 export type TRPCRouter = typeof trpcRouter
