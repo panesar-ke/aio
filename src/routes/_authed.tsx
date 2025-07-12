47e56bd08592695cc037c7c5696e1ddaf4c1dd26
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-07-12 10:47:55 +0300

    feat: layout for app

--- src/routes/_authed.tsx
+++ src/routes/_authed.tsx
@@ -1,4 +1,8 @@
+import { Suspense } from 'react'
 import { Outlet, createFileRoute, redirect } from '@tanstack/react-router'
+import { ErrorBoundary } from 'react-error-boundary'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import { AppSidebar, SidebarSkeleton } from '@/components/layout/app-sidebar'
 
 export const Route = createFileRoute('/_authed')({
   beforeLoad: ({ context, location }) => {
@@ -6,13 +10,34 @@ export const Route = createFileRoute('/_authed')({
       throw redirect({ to: '/login', search: { redirect: location.href } })
     }
   },
+  loader: async ({ context }) => {
+    await context.queryClient.prefetchQuery(context.trpc.forms.queryOptions())
+  },
   component: RouteComponent,
 })
 
 function RouteComponent() {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <SidebarProvider>
+      <ErrorBoundary
+        fallback={
+          <div role="alert">
+            <p>Something went wrong:</p>
+          </div>
+        }
+      >
+        <Suspense fallback={<SidebarSkeleton />}>
+          <AppSidebar />
+        </Suspense>
+      </ErrorBoundary>
+      <SidebarInset>
+        <header className="flex h-16 bg-background sticky top-0 shrink-0 items-center gap-2 border-b p-4">
+          <div className="ml-auto flex items-center gap-2 px-4"></div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <Outlet />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
