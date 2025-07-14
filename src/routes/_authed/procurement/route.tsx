27668f4bae558c07c7f29397b40e01998df73538
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-07-14 10:23:58 +0300

    refactor: remove trpc

--- src/routes/_authed/procurement/route.tsx
+++ src/routes/_authed/procurement/route.tsx
@@ -1,16 +1,13 @@
 import { Outlet, createFileRoute } from '@tanstack/react-router'
 import { ErrorNotification } from '@/components/custom/error-components'
+import { globalOptions } from '@/features/procurement/utils/query-options'
 
 export const Route = createFileRoute('/_authed/procurement')({
   component: RouteComponent,
   loader: async ({ context }) => {
     await Promise.all([
-      context.queryClient.prefetchQuery(
-        context.trpc.procurement.selectableProducts.queryOptions(),
-      ),
-      context.queryClient.prefetchQuery(
-        context.trpc.procurement.selectableProjects.queryOptions(),
-      ),
+      context.queryClient.prefetchQuery(globalOptions.selectableProducts()),
+      context.queryClient.prefetchQuery(globalOptions.selectableProjects()),
     ])
   },
   errorComponent: ({ error }) => <ErrorNotification message={error.message} />,
