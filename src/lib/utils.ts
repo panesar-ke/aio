716d459f0714469c68c939353101889b37c99d7c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-07-14 19:38:37 +0300

    refactor: reorganize imports and update function parameter types

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from 'clsx'
+import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import type { ClassValue } from 'clsx'
 
-export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: Array<ClassValue>) {
   return twMerge(clsx(inputs))
+}
+
+export function generateRandomId(prefix: string = 'id'): string {
+  return `${prefix}-${Math.random().toString(36).substring(2, 15)}`
 }
