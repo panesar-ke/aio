5b3b45f8aab0209f7c602f7c4ba2269f2f2eb699
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-07-15 15:18:32 +0300

    feat: react-pdf installation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import path from 'node:path'
+import fs from 'node:fs'
+
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+
+const pdfjsDistPath = path.dirname(require.resolve('pdfjs-dist/package.json'))
+const pdfWorkerPath = path.join(pdfjsDistPath, 'build', 'pdf.worker.mjs')
+
+fs.cpSync(pdfWorkerPath, './dist/pdf.worker.mjs', { recursive: true })
 
 const config = defineConfig({
   plugins: [
