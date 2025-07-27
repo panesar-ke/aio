75e28b35045cd4e342be7999990ed736cd570f0e
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-07-27 16:50:07 +0300

    feat: procurement types

--- src/features/procurement/utils/procurement.types.ts
+++ src/features/procurement/utils/procurement.types.ts
@@ -3,7 +3,16 @@ import type {
   getMaterialRequisitions,
   getRequisition,
 } from '@/features/procurement/services/server-fns'
-import type { materialRequisitionFormSchema } from '@/features/procurement/utils/schemas'
+import type {
+  materialRequisitionFormSchema,
+  orderSchema,
+} from '@/features/procurement/utils/schemas'
+import type {
+  getPendingRequests,
+  getPurchaseOrder,
+  getPurchaseOrders,
+} from '@/features/procurement/services/orders/server-fns'
+import type { UseFormReturn } from 'react-hook-form'
 
 export type MaterialRequisitionTableRow = Awaited<
   ReturnType<typeof getMaterialRequisitions>
@@ -13,3 +22,40 @@ export type MaterialRequisitionFormValues = z.infer<
 >
 
 export type Requisition = Awaited<ReturnType<typeof getRequisition>>
+
+export type OrderTableRow = Awaited<
+  ReturnType<typeof getPurchaseOrders>
+>[number]
+export type OrderFormValues = z.infer<typeof orderSchema>
+export type PendingOrder = Awaited<
+  ReturnType<typeof getPendingRequests>
+>[number]
+export interface OrderForm {
+  form: UseFormReturn<OrderFormValues>
+}
+
+export type Order = Awaited<ReturnType<typeof getPurchaseOrder>>
+
+interface OrderItem {
+  itemName: string
+  unit: string
+  quantity: number
+  unitPrice: number
+  discount: number
+  totalPrice: number
+}
+
+export interface OrderData {
+  orderNumber: string
+  orderDate: Date
+  supplierName: string
+  supplierAddress?: string | null
+  supplierEmail?: string | null
+  supplierPhone?: string | null
+  contactPerson?: string | null
+  taxPin?: string | null
+  vatType: OrderFormValues['vatType']
+  vatRate: number
+  items: Array<OrderItem>
+  userName: string
+}
