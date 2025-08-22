c6a556297b3496904d9b8f49400fe77e99928b7e
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-08-22 09:22:42 +0300

    chore: rights mid

--- src/app/(protected)/admin/rights/page.tsx
+++ src/app/(protected)/admin/rights/page.tsx
@@ -1,13 +1,23 @@
 import { RightsForm } from '@/features/admin/components/rights/rights-form';
+import { getForms, getUsers } from '@/features/admin/services/data';
+import { transformOptions } from '@/lib/helpers/formatters';
 
 export const metadata = {
   title: 'User rights',
 };
 
 export default async function UserRightsPage() {
+  const [forms, users] = await Promise.all([getForms(), getUsers()]);
   return (
     <div className="space-y-6">
-      <RightsForm />
+      <RightsForm
+        forms={forms}
+        users={transformOptions(
+          users
+            .filter(u => u.active && u.userType === 'STANDARD USER')
+            .map(user => ({ id: user.id, name: user.name.toUpperCase() }))
+        )}
+      />
     </div>
   );
 }
--- src/features/admin/services/data.ts
+++ src/features/admin/services/data.ts
@@ -1,21 +1,55 @@
 'use cache';
 import { unstable_cacheTag as cacheTag } from 'next/cache';
-import { getFormsGlobalTag } from '@/features/admin/utils/cache';
+import {
+  getFormsGlobalTag,
+  getUserFormsGlobalTag,
+  getUsersGlobalTag,
+} from '@/features/admin/utils/cache';
 import db from '@/drizzle/db';
 
+import { eq } from 'drizzle-orm';
 import { User } from '@/types/index.types';
+import { forms, userRights } from '@/drizzle/schema';
+
+export const getForms = async () => {
+  cacheTag(getFormsGlobalTag());
+  return await db.query.forms.findMany({
+    columns: { id: true, formName: true, path: true, module: true },
+    where: (forms, { eq }) => eq(forms.active, true),
+    orderBy: (forms, { asc }) => [asc(forms.moduleId), asc(forms.menuOrder)],
+  });
+};
+
+export const getUsers = async () => {
+  cacheTag(getUsersGlobalTag());
+  return await db.query.users.findMany({
+    columns: {
+      password: false,
+      contactVerified: false,
+      defaultMenu: false,
+      promptPasswordChange: false,
+      resetToken: false,
+    },
+    orderBy: (users, { asc }) => [asc(users.name)],
+  });
+};
+
 export const getUserForms = async (
   userId: string,
   userType: User['userType']
 ) => {
-  cacheTag(getFormsGlobalTag(userId));
-  if (userType === 'ADMIN') {
-    return await db.query.forms.findMany({
-      columns: { id: true, formName: true, path: true, module: true },
-      where: (forms, { eq }) => eq(forms.active, true),
-      orderBy: (forms, { asc }) => [asc(forms.moduleId), asc(forms.menuOrder)],
-    });
-  }
+  cacheTag(getUserFormsGlobalTag(userId));
+
+  //   return db
+  //     .select({
+  //       id: forms.id,
+  //       formName: forms.formName,
+  //       path: forms.path,
+  //       module: forms.module,
+  //     })
+  //     .from(forms)
+  //     .innerJoin(userRights, eq(userRights.formId, forms.id));
+  //   // .where(userType === 'ADMIN' ? undefined : eq(userRights.userId, userId));
 
   // TODO: refactor this
   return await db.query.forms.findMany({
