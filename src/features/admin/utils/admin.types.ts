bbdcdd2ef1ae06a219f7219b0dee12a9331c5f43
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-08-22 07:58:34 +0300

    chore: add users to AdminCacheTag

--- src/features/admin/utils/admin.types.ts
+++ src/features/admin/utils/admin.types.ts
@@ -3,4 +3,4 @@ import { userRightsFormSchema } from '@/features/admin/utils/schema';
 
 export type UserRightsFormValue = z.infer<typeof userRightsFormSchema>;
 
-export type AdminCacheTag = 'forms';
+export type AdminCacheTag = 'forms' | 'users';
