e649c5fb0323aab5b8142608fb25b10e8fb9336a
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-08-22 14:14:42 +0300

    chore: add type for clone user right form from schema

--- src/features/admin/utils/admin.types.ts
+++ src/features/admin/utils/admin.types.ts
@@ -1,6 +1,12 @@
 import z from 'zod';
-import { userRightsFormSchema } from '@/features/admin/utils/schema';
+import {
+  cloneUserRightsFormSchema,
+  userRightsFormSchema,
+} from '@/features/admin/utils/schema';
 
 export type UserRightsFormValue = z.infer<typeof userRightsFormSchema>;
+export type CloneUserRightsFormValues = z.infer<
+  typeof cloneUserRightsFormSchema
+>;
 
 export type AdminCacheTag = 'forms' | 'users';
