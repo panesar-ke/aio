a26315205c99cfa186ba0ab16e5c2015a3e2d0d6
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-08-27 16:30:10 +0300

    chore: schema and type for grn

--- src/features/store/utils/schema.ts
+++ src/features/store/utils/schema.ts
@@ -1,7 +1,33 @@
-import { requiredStringSchemaEntry } from '@/lib/schema-rules';
 import { z } from 'zod';
+import {
+  optionalNumberSchemaEntry,
+  optionalStringSchemaEntry,
+  requiredDateSchemaEntry,
+  requiredNumberSchemaEntry,
+  requiredStringSchemaEntry,
+} from '@/lib/schema-rules';
 
 export const storeFormSchema = z.object({
   storeName: requiredStringSchemaEntry('Store name is required'),
   description: requiredStringSchemaEntry('Description is required'),
+});
+
+export const grnFormSchema = z.object({
+  receiptDate: requiredDateSchemaEntry(),
+  orderId: requiredStringSchemaEntry('Order is required'),
+  invoiceNo: optionalStringSchemaEntry(),
+  vendorId: requiredStringSchemaEntry('Vendor is required'),
+  vendorName: requiredStringSchemaEntry('Vendor is required'),
+  storeId: requiredStringSchemaEntry('Store is required'),
+  items: z.array(
+    z.object({
+      id: requiredStringSchemaEntry('ID is required'),
+      itemId: requiredStringSchemaEntry('Item is required'),
+      productName: requiredStringSchemaEntry('Item is required'),
+      orderedQty: requiredNumberSchemaEntry('ordered Qty is required'),
+      qty: requiredNumberSchemaEntry('Qty must be a positive number'),
+      rate: optionalNumberSchemaEntry(),
+      remarks: optionalStringSchemaEntry(),
+    })
+  ),
 });
--- src/features/store/utils/store.types.ts
+++ src/features/store/utils/store.types.ts
@@ -1,8 +1,26 @@
 import type { z } from 'zod';
 import type { getStores } from '@/features/store/services/stores/data';
-import type { storeFormSchema } from '@/features/store/utils/schema';
+import type {
+  grnFormSchema,
+  storeFormSchema,
+} from '@/features/store/utils/schema';
 
-export type StoreCacheTags = 'stores' | 'grns' | 'material issues';
+export type StoreCacheTags =
+  | 'stores'
+  | 'grns'
+  | 'grn number'
+  | 'material issues'
+  | 'unreceived orders';
 
 export type Store = Awaited<ReturnType<typeof getStores>>[number];
 export type StoreFormValues = z.infer<typeof storeFormSchema>;
+
+export type GrnFormValues = z.infer<typeof grnFormSchema>;
+
+export type StockMovementType =
+  | 'GRN'
+  | 'ISSUE'
+  | 'TRANSFER'
+  | 'CONVERSION'
+  | 'CONVERSION_IN'
+  | 'CONVERSION_OUT';
