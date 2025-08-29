b29c4eaa17e55a5d41ac0675c48921f1a2f45aed
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-08-29 14:44:53 +0300

    chore: schema and types for material transfer

--- src/features/store/utils/store.types.ts
+++ src/features/store/utils/store.types.ts
@@ -2,6 +2,7 @@ import type { z } from 'zod';
 import type { getStores } from '@/features/store/services/stores/data';
 import type {
   grnFormSchema,
+  materialTransferFormSchema,
   storeFormSchema,
 } from '@/features/store/utils/schema';
 
@@ -10,12 +11,17 @@ export type StoreCacheTags =
   | 'grns'
   | 'grn number'
   | 'material issues'
-  | 'unreceived orders';
+  | 'unreceived orders'
+  | 'transfers';
 
 export type Store = Awaited<ReturnType<typeof getStores>>[number];
 export type StoreFormValues = z.infer<typeof storeFormSchema>;
 
 export type GrnFormValues = z.infer<typeof grnFormSchema>;
+
+export type MaterialTransferFormValues = z.infer<
+  typeof materialTransferFormSchema
+>;
 
 export type StockMovementType =
   | 'GRN'
@@ -23,4 +29,5 @@ export type StockMovementType =
   | 'TRANSFER'
   | 'CONVERSION'
   | 'CONVERSION_IN'
-  | 'CONVERSION_OUT';
+  | 'CONVERSION_OUT'
+  | 'TRANSFER_IN';
