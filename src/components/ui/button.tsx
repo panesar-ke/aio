e0436d73a319d5db9c5b66762eb9d60e8785c203
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-01-11 15:44:10 +0300

    add ButtonLink

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,6 +2,8 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import type { Route } from 'next';
 
 const buttonVariants = cva(
   "cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -61,4 +63,21 @@ function Button({
   );
 }
 
-export { Button, buttonVariants };
+function ButtonLink({
+  path,
+  variant = 'secondary',
+  size = 'sm',
+  ...props
+}: React.ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    path: Route;
+    children: React.ReactNode;
+  }) {
+  return (
+    <Button variant={variant} size={size} asChild {...props}>
+      <Link href={path}>{props.children}</Link>
+    </Button>
+  );
+}
+
+export { Button, buttonVariants, ButtonLink };
