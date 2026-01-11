0aa0a9ca58ccac05648a574c6cbe97f478205424
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-01-11 09:04:42 +0300

    function for generating user password

---
+++ src/features/admin/utils/helpers.ts
@@ -0,0 +1,10 @@
+export const generatePassword = (length: number) => {
+  const characters =
+    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$';
+  let password = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+  return password;
+};
