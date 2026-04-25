6d419cf01c71f410c03975cad2f4769558bf3a41
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-04-25 16:57:58 +0300

    refactor to accomodate custom search handler

--- src/components/custom/search.tsx
+++ src/components/custom/search.tsx
@@ -1,16 +1,21 @@
 'use client';
 
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { SearchIcon } from 'lucide-react';
-import { useDebounceCallback } from 'usehooks-ts';
-import { cn } from '@/lib/utils';
 import type { Route } from 'next';
+
+import { SearchIcon } from 'lucide-react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebounceCallback } from 'usehooks-ts';
+
+import { normalizeString } from '@/lib/string-normalizers';
+import { cn } from '@/lib/utils';
 
 interface SearchProps {
   placeholder: string;
   className?: string;
   allowOnlySearch?: boolean;
   parentClassName?: string;
+  onHandleSearch?: (value: string) => void;
+  defaultValue?: string;
 }
 
 export default function Search({
@@ -18,15 +23,21 @@ export default function Search({
   className,
   allowOnlySearch,
   parentClassName,
+  onHandleSearch,
+  defaultValue,
 }: SearchProps) {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const pathname = usePathname();
 
   const handleSearch = useDebounceCallback((term: string) => {
+    if (onHandleSearch) {
+      onHandleSearch(term);
+      return;
+    }
     const params = new URLSearchParams(searchParams);
     if (term) {
-      params.set('search', term);
+      params.set('search', normalizeString(term));
     } else {
       params.delete('search');
     }
@@ -53,11 +64,11 @@ export default function Search({
           'focus-visible:border-ring focus-visible:ring-ring/50 ',
           'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
           'pl-10',
-          className
+          className,
         )}
         onChange={e => handleSearch(e.target.value)}
         placeholder={placeholder}
-        defaultValue={searchParams.get('search')?.toString()}
+        defaultValue={defaultValue ?? searchParams.get('search')?.toString()}
         type="search"
       />
       <SearchIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
