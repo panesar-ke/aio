1d222356020d6622bc82ab879fcb7b8dc6105143
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-08-02 22:17:59 +0300

    remove `sendSupplierPoEmail`

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,11 +1,10 @@
-import { serve } from 'inngest/next';
+import { serve } from "inngest/next";
 
-import { inngest } from '@/inngest/client';
-import { sendSupplierPoEmail } from '@/inngest/functions/procurement';
-import { deactivateStaleProducts } from '@/inngest/functions/store';
-import { sendUserNewPassword } from '@/inngest/functions/users';
+import { inngest } from "@/inngest/client";
+import { deactivateStaleProducts } from "@/inngest/functions/store";
+import { sendUserNewPassword } from "@/inngest/functions/users";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [sendSupplierPoEmail, sendUserNewPassword, deactivateStaleProducts],
+  functions: [sendUserNewPassword, deactivateStaleProducts],
 });
